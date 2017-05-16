1d5526aee3f0ba473f8180d7e5b8324dd8d9b6e3
Austin Tindle <tindleaj@gmail.com>
2017-05-16 12:45:57 -0400

    added schedules to sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,14 @@
 
 <h5>Contact</h5>
 <p>
-<a href="mailto:Andre.Gesquiere@ucf.edu ">Andre Gesquiere</a><br>
-<a href="mailto:Sudipta.Seal@ucf.edu ">Sudipta Seal</a>
+	<a href="mailto:Andre.Gesquiere@ucf.edu ">Andre Gesquiere</a><br>
+	<a href="mailto:Sudipta.Seal@ucf.edu ">Sudipta Seal</a>
 </p>
+
+<h5>Schedules</h5>
+<p>
+	<a href="../files/schedule.docx">REU Schedule</a>
+	<a href="../files/wac-schedule.docx">REU WAC Schedule</a>
+</p>
+
 
