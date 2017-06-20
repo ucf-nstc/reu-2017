042a94e9e606a86eef48a3590c22bcae50661a13
Austin Tindle <tindleaj@gmail.com>
2017-06-20 09:44:50 -0400

    Add potluck 2017 album

--- gallery.php
+++ gallery.php
@@ -21,6 +21,10 @@ require_once('includes/functions.php');
         <h4>Gallery</h4>
         <h5>REU 2017</h5>
         <a href="./images/group.jpg"><img class="album-thumb" src="./images/group.jpg"></a>
+
+        <h5>REU Picnic - Potluck 2017</h5>
+        <?php display_album('potluck-2017/'); ?>
+
         <h5>REU 2016</h5>
         <?php display_album('reu-2016/'); ?>
     </div>
